f3ef3f1d3d8a270289dc342bc60ef17f11cf77e3
hiepnxph15148 <hiepnxph15148@fpt.edu.vn>
2024-09-12 10:40:09 +0700

    khái niệm về useReducer và bài tập cơ bản

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,10 +1,13 @@
-export default function Cart({ items, onUpdateItemQuantity }) {
+import { useContext } from "react";
+import { CartContext } from "../store/shopping-cart-context";
+
+export default function Cart() {
+  const { items, updateItemQuantity } = useContext(CartContext);
   const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
   );
   const formattedTotalPrice = `$${totalPrice.toFixed(2)}`;
-
   return (
     <div id="cart">
       {items.length === 0 && <p>No items in cart!</p>}
@@ -20,11 +23,11 @@ export default function Cart({ items, onUpdateItemQuantity }) {
                   <span> ({formattedPrice})</span>
                 </div>
                 <div className="cart-item-actions">
-                  <button onClick={() => onUpdateItemQuantity(item.id, -1)}>
+                  <button onClick={() => updateItemQuantity(item.id, -1)}>
                     -
                   </button>
                   <span>{item.quantity}</span>
-                  <button onClick={() => onUpdateItemQuantity(item.id, 1)}>
+                  <button onClick={() => updateItemQuantity(item.id, 1)}>
                     +
                   </button>
                 </div>
